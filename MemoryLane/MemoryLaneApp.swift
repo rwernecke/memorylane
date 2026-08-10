82d06f9ee9b179de5cf753eebda774a38bd7fd24
rw_dreaming <61444856+rwernecke@users.noreply.github.com>
2026-08-09 23:57:33 -0700

    Simplify app entry for photo deck

--- MemoryLane/MemoryLaneApp.swift
+++ MemoryLane/MemoryLaneApp.swift
@@ -6,31 +6,12 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct MemoryLaneApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Memory.self,
-        ])
-        let modelConfiguration = ModelConfiguration(
-            "MemoryLaneStore",
-            schema: schema,
-            isStoredInMemoryOnly: false
-        )
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
